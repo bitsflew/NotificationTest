4d7f617076b320bb24cec347855f6903f6741c50
Henk Kampman <henk.kampman@secondmove.com>
2024-10-21 15:27:24 +0200

    Initial Commit

---
+++ NotificationTest/NotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationTestApp.swift
+//  NotificationTest
+//
+//  Created by Henk on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotificationTestTests/NotificationTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationTestTests.swift
+//  NotificationTestTests
+//
+//  Created by Henk on 21/10/2024.
+//
+
+import Testing
+@testable import NotificationTest
+
+struct NotificationTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
